d04dab435e1fad32423f20dbdbb495e3c3d9ab5f
ethanhopkins <112411800+ethanhopkins@users.noreply.github.com>
2023-01-23 14:45:21 -0500

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,19 @@
+/**
+ * The program will calculate the area of a square and circle through the use of different methods.
+ * @author Ethan Hopkins
+ * @version 1.0
+ * Homework 1
+ * Spring Semester/Freshman Year
+ */
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Square sq1 = new Square();
+		System.out.println("Square area: " + sq1.area());
+		
+		Circle c1 = new Circle();
+		System.out.println("Circle area: " + c1.area());
+	}
+
+}
