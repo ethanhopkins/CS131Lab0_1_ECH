e6d534d95cd45df371f23d54513f781dcc5a6dad
ethanhopkins <112411800+ethanhopkins@users.noreply.github.com>
2023-01-26 15:34:37 -0500

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,33 +1,57 @@
 /**
- * The program will calculate the area of a square and circle through the use of different methods.
+ * The program will calculate the area of a circle
  * @author Ethan Hopkins
  * @version 1.0
  * Homework 1
  * Spring Semester/Freshman Year
  */
-//preferred constructor
+
 public class Circle {
-	double radius = 1;
+	double radius;//radius will be used to determine the area of the circle.
 	
 	
-	//getter
-	public double getRadius(double radius) {
-		return radius;
-	}
-	
-	//setter
-	public void setRadius(double newRadius) {
-		radius = newRadius;
-	}
+	/**
+	 * The default constructor will inititalize radius to a value of 1.
+	 */
+	public Circle() {
+		radius = 1;
+	}//end default constructor
 	
 	
-	//area method
+	/**
+	 * 
+	 * @param radius; the value of radius will be 0 if any value is specified.
+	 */
+	public Circle(double radius) {
+		radius = 0;
+	}//end preferred constructor
+	
+	
+	/**
+	 * Use the formula for the area of a circle with the radius to find 
+	 * the value of the area.
+	 * 
+	 * @return the area.
+	 */
 	public double area() {
 		return Math.PI*Math.pow(radius, 2);
-	}
+	}//end area method
 	
-	//default constructor
-	public Circle() {
-		double radius = 0;
-	}
-}
+	
+	/**
+	 * @return the value of radius.
+	 */
+	public double getRadius() {
+		return radius;
+	}//end getter
+	
+	
+	/**
+	 * 
+	 * @param newRadius; the value of radius to be set.
+	 */
+	public void setRadius(double newRadius) {
+		radius = newRadius;
+	}//end setter
+	
+}//end class
